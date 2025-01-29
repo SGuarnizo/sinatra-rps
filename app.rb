0f0a863bb21e5d7c2f81b3c9ad3740d12282c2d2
SGuarnizo <sguarniz@chicagobooth.edu>
2025-01-29 19:44:41 +0000

    final

--- app.rb
+++ app.rb
@@ -4,15 +4,11 @@ require "better_errors"
 require "binding_of_caller"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
 end
 
 get("/rock") do
   @random = rand(1..3)
-
   if @random == 1
     @they = "rock"
     @outcome = "tied"
@@ -20,11 +16,47 @@ get("/rock") do
     @they = "paper"
     @outcome = "lost"
   else
-    @they = "paper"
+    @they = "scissors"
     @outcome = "won"
   end
   @we_play = "We played rock!"
   @they_play = "They played #{@they}!"
   @outcome_mssg = "We #{@outcome}!"
 erb(:rock)
+end
+
+get("/paper") do
+  @random = rand(1..3)
+  if @random == 1
+    @they = "rock"
+    @outcome = "won"
+  elsif @random == 2
+    @they = "paper"
+    @outcome = "tied"
+  else
+    @they = "scissors"
+    @outcome = "lost"
+  end
+  @we_play = "We played paper!"
+  @they_play = "They played #{@they}!"
+  @outcome_mssg = "We #{@outcome}!"
+erb(:paper)
+end
+
+get("/scissors") do
+  @random = rand(1..3)
+  if @random == 1
+    @they = "rock"
+    @outcome = "lost"
+  elsif @random == 2
+    @they = "paper"
+    @outcome = "won"
+  else
+    @they = "scissors"
+    @outcome = "tied"
+  end
+  @we_play = "We played scissors!"
+  @they_play = "They played #{@they}!"
+  @outcome_mssg = "We #{@outcome}!"
+erb(:scissors)
 end
